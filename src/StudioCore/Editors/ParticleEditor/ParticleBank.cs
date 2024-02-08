af43b97be6b9ee6c7344496a6ceeb8b49fb0316f
Vawser <ben.rouncefield@gmail.com>
2024-02-08 22:51:09 +0000

    Particle Editor
    - Added Particle Editor framework

--- src/StudioCore/Editors/ParticleEditor/ParticleBank.cs
+++ src/StudioCore/Editors/ParticleEditor/ParticleBank.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using SoulsFormats;
+using StudioCore.Locators;
+using StudioCore.UserProject;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,5 +12,174 @@ using System.Threading.Tasks;
 namespace StudioCore.Editors.ParticleEditor;
 public static class ParticleBank
 {
+    public static bool IsLoaded { get; private set; }
+    public static bool IsLoading { get; private set; }
 
+    public static Dictionary<ParticleFileInfo, IBinder> FileBank { get; private set; } = new();
+
+    public static void SaveParticles()
+    {
+        foreach (var (info, binder) in FileBank)
+        {
+            SaveParticle(info, binder);
+        }
+    }
+
+    public static void SaveParticle(ParticleFileInfo info, IBinder binder)
+    {
+        //TaskLogs.AddLog($"SaveParticle: {info.Path}");
+
+        var fileDir = @"\sfx";
+        var fileExt = @".ffxbnd.dcx";
+
+        foreach (BinderFile file in binder.Files)
+        {
+            foreach (FXR3 cFile in info.ParticleFiles)
+            {
+                file.Bytes = cFile.Write();
+            }
+        }
+
+        BND4 writeBinder = binder as BND4;
+        byte[] fileBytes = null;
+
+        var assetRoot = $@"{Project.GameRootDirectory}\{fileDir}\{info.Name}{fileExt}";
+        var assetMod = $@"{Project.GameModDirectory}\{fileDir}\{info.Name}{fileExt}";
+
+        switch (Project.Type)
+        {
+            case ProjectType.DS3:
+                fileBytes = writeBinder.Write(DCX.Type.DCX_DFLT_10000_44_9);
+                break;
+            case ProjectType.SDT:
+                fileBytes = writeBinder.Write(DCX.Type.DCX_KRAK);
+                break;
+            case ProjectType.ER:
+                fileBytes = writeBinder.Write(DCX.Type.DCX_KRAK);
+                break;
+            case ProjectType.AC6:
+                fileBytes = writeBinder.Write(DCX.Type.DCX_KRAK_MAX);
+                break;
+            default:
+                TaskLogs.AddLog($"Invalid ProjectType during SaveCutscene");
+                return;
+        }
+
+        // Add folder if it does not exist in GameModDirectory
+        if (!Directory.Exists($"{Project.GameModDirectory}\\{fileDir}\\"))
+        {
+            Directory.CreateDirectory($"{Project.GameModDirectory}\\{fileDir}\\");
+        }
+
+        // Make a backup of the original file if a mod path doesn't exist
+        if (Project.GameModDirectory == null && !File.Exists($@"{assetRoot}.bak") && File.Exists(assetRoot))
+        {
+            File.Copy(assetRoot, $@"{assetRoot}.bak", true);
+        }
+
+        if (fileBytes != null)
+        {
+            File.WriteAllBytes(assetMod, fileBytes);
+            //TaskLogs.AddLog($"Saved at: {assetMod}");
+        }
+    }
+
+    public static void LoadParticles()
+    {
+        if (Project.Type == ProjectType.Undefined)
+        {
+            return;
+        }
+
+        IsLoaded = false;
+        IsLoading = true;
+
+        FileBank = new();
+
+        var fileDir = @"\sfx";
+        var fileExt = @".ffxbnd.dcx";
+
+        List<string> fileNames = FileAssetLocator.GetParticleBinders();
+
+        foreach (var name in fileNames)
+        {
+            var filePath = $"{fileDir}\\{name}{fileExt}";
+
+            if (File.Exists($"{Project.GameModDirectory}\\{filePath}"))
+            {
+                LoadParticle($"{Project.GameModDirectory}\\{filePath}");
+                //TaskLogs.AddLog($"Loaded from GameModDirectory: {filePath}");
+            }
+            else
+            {
+                LoadParticle($"{Project.GameRootDirectory}\\{filePath}");
+                //TaskLogs.AddLog($"Loaded from GameRootDirectory: {filePath}");
+            }
+        }
+
+        IsLoaded = true;
+        IsLoading = false;
+
+        TaskLogs.AddLog($"Particle File Bank - Load Complete");
+    }
+
+    public static void LoadParticle(string path)
+    {
+        if (path == null)
+        {
+            TaskLogs.AddLog($"Could not locate {path} when loading ffx file.",
+                    LogLevel.Warning);
+            return;
+        }
+        if (path == "")
+        {
+            TaskLogs.AddLog($"Could not locate {path} when loading ffx file.",
+                    LogLevel.Warning);
+            return;
+        }
+
+        var name = Path.GetFileNameWithoutExtension(Path.GetFileNameWithoutExtension(path));
+        ParticleFileInfo fileStruct = new ParticleFileInfo(name, path);
+
+        IBinder binder = BND4.Read(DCX.Decompress(path));
+
+        foreach (var file in binder.Files)
+        {
+            if (file.Name.Contains(".fxr"))
+            {
+                try
+                {
+                    FXR3 cFile = FXR3.Read(file.Bytes);
+                    fileStruct.ParticleFiles.Add(cFile);
+                }
+                catch (Exception ex)
+                {
+                    TaskLogs.AddLog($"{file.ID} - Failed to read.\n{ex.ToString()}");
+                }
+            }
+        }
+
+        FileBank.Add(fileStruct, binder);
+    }
+
+    public struct ParticleFileInfo
+    {
+        public ParticleFileInfo(string name, string path)
+        {
+            Name = name;
+            Path = path;
+            Modified = false;
+            Added = false;
+            ParticleFiles = new List<FXR3>();
+        }
+
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+        public bool Modified { get; set; }
+
+        public bool Added { get; set; }
+
+        public List<FXR3> ParticleFiles { get; set; }
+    }
 }
--- src/StudioCore/Editors/ParticleEditor/ParticleEditorScreen.cs
+++ src/StudioCore/Editors/ParticleEditor/ParticleEditorScreen.cs
@@ -1,14 +1,12 @@
 ﻿using ImGuiNET;
 using SoulsFormats;
-using StudioCore.Configuration;
 using StudioCore.Editor;
-using StudioCore.Settings;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using StudioCore.Editors.ParticleEditor;
+using StudioCore.UserProject;
 using System.Numerics;
 using Veldrid;
 using Veldrid.Sdl2;
+using static StudioCore.Editors.ParticleEditor.ParticleBank;
 
 namespace StudioCore.ParticleEditor;
 
@@ -18,6 +16,13 @@ public class ParticleEditorScreen : EditorScreen
     private ProjectSettings _projectSettings;
 
     public ActionManager EditorActionManager = new();
+
+    private ParticleFileInfo _selectedFileInfo;
+    private IBinder _selectedBinder;
+    private string _selectedBinderKey;
+
+    private FXR3 _selectedParticle;
+    private int _selectedParticleKey;
 
     public ParticleEditorScreen(Sdl2Window window, GraphicsDevice device)
     {
@@ -34,23 +39,109 @@ public class ParticleEditorScreen : EditorScreen
 
     public void OnGUI(string[] initcmd)
     {
+        var scale = Smithbox.GetUIScale();
+
+        // Docking setup
+        ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Vector2(4, 4) * scale);
+        Vector2 wins = ImGui.GetWindowSize();
+        Vector2 winp = ImGui.GetWindowPos();
+        winp.Y += 20.0f * scale;
+        wins.Y -= 20.0f * scale;
+        ImGui.SetNextWindowPos(winp);
+        ImGui.SetNextWindowSize(wins);
+
+        if (Project.Type is ProjectType.DS1 or ProjectType.DS1R or ProjectType.BB or ProjectType.DS2S)
+        {
+            ImGui.Text($"This editor does not support {Project.Type}.");
+            ImGui.PopStyleVar();
+            return;
+        }
+        else if (_projectSettings == null)
+        {
+            ImGui.Text("No project loaded. File -> New Project");
+        }
+
+        var dsid = ImGui.GetID("DockSpace_ParticleEditor");
+        ImGui.DockSpace(dsid, new Vector2(0, 0), ImGuiDockNodeFlags.None);
+
+        if (ParticleBank.IsLoaded)
+        {
+            ParticleFileView();
+        }
+
+        ImGui.PopStyleVar();
+    }
+
+    public void ParticleFileView()
+    {
+        // File List
+        ImGui.Begin("Files##ParticleFileList");
+
+        ImGui.Text($"File");
+        ImGui.Separator();
+
+        foreach (var (info, binder) in ParticleBank.FileBank)
+        {
+            // Ignore the resource ffxbnd files
+            if (info.ParticleFiles.Count > 0)
+            {
+                if (ImGui.Selectable($@" {info.Name}", info.Name == _selectedBinderKey))
+                {
+                    _selectedParticleKey = -1; // Clear particle key if file is changed
+
+                    _selectedBinderKey = info.Name;
+                    _selectedFileInfo = info;
+                    _selectedBinder = binder;
+                }
+            }
+        }
+
+        ImGui.End();
+
+        // File List
+        ImGui.Begin("Particles##ParticleList");
+
+        if (_selectedFileInfo.ParticleFiles != null)
+        {
+            ImGui.Text($"Particles");
+            ImGui.Separator();
+
+            for (int i = 0; i < _selectedFileInfo.ParticleFiles.Count; i++)
+            {
+                FXR3 entry = _selectedFileInfo.ParticleFiles[i];
+
+                if (ImGui.Selectable($@" {entry.ID}", i == _selectedParticleKey))
+                {
+                    _selectedParticleKey = i;
+                    _selectedParticle = entry;
+                }
+            }
+        }
+
+        ImGui.End();
     }
 
     public void OnProjectChanged(ProjectSettings newSettings)
     {
         _projectSettings = newSettings;
 
+        // Only support FXR3 for now
+        if(Project.Type is ProjectType.DS3 or ProjectType.SDT or ProjectType.ER or ProjectType.AC6)
+        {
+            ParticleBank.LoadParticles();
+        }
+
         ResetActionManager();
     }
 
     public void Save()
     {
-
+        ParticleBank.SaveParticle(_selectedFileInfo, _selectedBinder);
     }
 
     public void SaveAll()
     {
-
+        ParticleBank.SaveParticles();
     }
 
     private void ResetActionManager()
