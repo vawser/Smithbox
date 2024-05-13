f04f1da9fc97388b0ff6ea6321a4ab97f32c273f
Vawser <ben.rouncefield@gmail.com>
2024-05-13 21:18:59 +0100

    feat: AC6 Paramdex

--- src/StudioCore/Configuration/KeyBindings.cs
+++ src/StudioCore/Configuration/KeyBindings.cs
@@ -111,10 +111,10 @@ public class KeyBindings
         public KeyBind ToggleWindow_Help = new("Toggle Help Window", KeybindCategory.Window, Key.F2);
         public KeyBind ToggleWindow_Keybind = new("Toggle Keybind Window", KeybindCategory.Window, Key.F3);
         public KeyBind ToggleWindow_Memory = new("Toggle Memory Window", KeybindCategory.Window, Key.F4);
-        public KeyBind ToggleWindow_Settings = new("Toggle Settings Window", KeybindCategory.Window, Key.F5);
-        public KeyBind ToggleWindow_Alias = new("Toggle Alias Window", KeybindCategory.Window, Key.F6);
-        public KeyBind ToggleWindow_ColorPicker = new("Toggle Color Picker Window", KeybindCategory.Window, Key.F7);
-        public KeyBind ToggleWindow_Debug = new("Toggle Debug Window", KeybindCategory.Window, Key.F8);
+        public KeyBind ToggleWindow_Settings = new("Toggle Settings Window", KeybindCategory.Window, Key.F6);
+        public KeyBind ToggleWindow_Alias = new("Toggle Alias Window", KeybindCategory.Window, Key.F7);
+        public KeyBind ToggleWindow_ColorPicker = new("Toggle Color Picker Window", KeybindCategory.Window, Key.F8);
+        public KeyBind ToggleWindow_Debug = new("Toggle Debug Window", KeybindCategory.Window, Key.F9);
 
         // Map Toolbar
         public KeyBind Toolbar_Rotate_X = new("Rotate X", KeybindCategory.MapEditor, Key.J);
@@ -163,7 +163,7 @@ public class KeyBindings
         public KeyBind Param_GotoBack = new("Go to Back", KeybindCategory.ParamEditor, Key.Escape);
         public KeyBind Param_GotoRowID = new("Go to Row ID", KeybindCategory.ParamEditor, Key.G, true);
         public KeyBind Param_GotoSelectedRow = new("Go to Selected Row", KeybindCategory.ParamEditor, Key.G);
-        public KeyBind Param_HotReload = new("Hot Reload", KeybindCategory.ParamEditor, Key.F5, true);
+        public KeyBind Param_HotReload = new("Hot Reload", KeybindCategory.ParamEditor, Key.F5);
         public KeyBind Param_HotReloadAll = new("Hot Reload All", KeybindCategory.ParamEditor, Key.F5, false, false, true);
         public KeyBind Param_ImportCSV = new("Import CSV", KeybindCategory.ParamEditor);
         public KeyBind Param_MassEdit = new("Mass Edit", KeybindCategory.ParamEditor);
