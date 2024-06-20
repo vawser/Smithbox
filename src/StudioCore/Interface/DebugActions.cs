96276bf0cb2830aa98e1f57c877390e188aedb30
Vawser <ben.rouncefield@gmail.com>
2024-06-20 22:29:16 +0100

    feat: Paramdex + Validation Action
    - Fixed some scuffedness with the Names
    - Added a paramdef validation action to the Debug menu, loads and checks the paramdef against the old PARAM implementation, logging issues that Andre.Format misses.

--- src/StudioCore/Interface/DebugActions.cs
+++ src/StudioCore/Interface/DebugActions.cs
@@ -96,6 +96,8 @@ public static class DebugActions
                 TaskLogs.AddLog(message,
                         LogLevel.Warning, TaskLogs.LogPriority.Normal, e);
             }
+
+            TaskLogs.AddLog($"{paramName} validated");
         }
     }
 
