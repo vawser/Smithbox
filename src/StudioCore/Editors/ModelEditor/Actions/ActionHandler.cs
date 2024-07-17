951670fb5e6b5a80ef1159f2db650f2abf4ded30
Vawser <ben.rouncefield@gmail.com>
2024-07-17 02:39:06 +0100

    fix: Model Editor - Actions
    - Fixed an issue where the Duplicate/Remove action would occur even if the selected index was -1 (invalid)

--- src/StudioCore/Editors/ModelEditor/Actions/ActionHandler.cs
+++ src/StudioCore/Editors/ModelEditor/Actions/ActionHandler.cs
@@ -78,56 +78,64 @@ namespace StudioCore.Editors.ModelEditor.Actions
                     if (Screen.ModelHierarchy.DummyMultiselect.HasValidMultiselection())
                         action = new DuplicateDummyEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.DummyMultiselect);
                     else
-                        action = new DuplicateDummyEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedDummy);
+                        if (Screen.ModelHierarchy._selectedDummy != -1)
+                            action = new DuplicateDummyEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedDummy);
                     break;
 
                 case ModelEntrySelectionType.Material:
                     if (Screen.ModelHierarchy.MaterialMultiselect.HasValidMultiselection())
                         action = new DuplicateMaterialEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.MaterialMultiselect);
                     else
-                        action = new DuplicateMaterialEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedMaterial);
+                        if (Screen.ModelHierarchy._selectedMaterial != -1)
+                            action = new DuplicateMaterialEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedMaterial);
                     break;
 
                 case ModelEntrySelectionType.GXList:
                     if (Screen.ModelHierarchy.GxListMultiselect.HasValidMultiselection())
                         action = new DuplicateGXListEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.GxListMultiselect);
                     else
-                        action = new DuplicateGXListEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedGXList);
+                        if (Screen.ModelHierarchy._selectedGXList != -1)
+                            action = new DuplicateGXListEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedGXList);
                     break;
 
                 case ModelEntrySelectionType.Node:
                     if (Screen.ModelHierarchy.NodeMultiselect.HasValidMultiselection())
                         action = new DuplicateNodeEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.NodeMultiselect);
                     else
-                        action = new DuplicateNodeEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedNode);
+                        if (Screen.ModelHierarchy._selectedNode != -1)
+                            action = new DuplicateNodeEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedNode);
                     break;
 
                 case ModelEntrySelectionType.Mesh:
                     if (Screen.ModelHierarchy.MeshMultiselect.HasValidMultiselection())
                         action = new DuplicateMeshEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.MeshMultiselect);
                     else
-                        action = new DuplicateMeshEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedMesh);
+                        if (Screen.ModelHierarchy._selectedMesh != -1)
+                            action = new DuplicateMeshEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedMesh);
                     break;
 
                 case ModelEntrySelectionType.BufferLayout:
                     if (Screen.ModelHierarchy.BufferLayoutMultiselect.HasValidMultiselection())
                         action = new DuplicateBufferLayoutEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.BufferLayoutMultiselect);
                     else
-                        action = new DuplicateBufferLayoutEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedBufferLayout);
+                        if (Screen.ModelHierarchy._selectedBufferLayout != -1)
+                            action = new DuplicateBufferLayoutEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedBufferLayout);
                     break;
 
                 case ModelEntrySelectionType.BaseSkeleton:
                     if (Screen.ModelHierarchy.BaseSkeletonMultiselect.HasValidMultiselection())
                         action = new DuplicateBaseSkeletonBoneEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.BaseSkeletonMultiselect);
                     else
-                        action = new DuplicateBaseSkeletonBoneEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedBaseSkeletonBone);
+                        if (Screen.ModelHierarchy._selectedBaseSkeletonBone != -1)
+                            action = new DuplicateBaseSkeletonBoneEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedBaseSkeletonBone);
                     break;
 
                 case ModelEntrySelectionType.AllSkeleton:
                     if (Screen.ModelHierarchy.AllSkeletonMultiselect.HasValidMultiselection())
                         action = new DuplicateAllSkeletonBoneEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.AllSkeletonMultiselect);
                     else
-                        action = new DuplicateAllSkeletonBoneEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedAllSkeletonBone);
+                        if (Screen.ModelHierarchy._selectedAllSkeletonBone != -1)
+                            action = new DuplicateAllSkeletonBoneEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedAllSkeletonBone);
                     break;
             }
 
@@ -150,56 +158,64 @@ namespace StudioCore.Editors.ModelEditor.Actions
                     if (Screen.ModelHierarchy.DummyMultiselect.HasValidMultiselection())
                         action = new RemoveDummyEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.DummyMultiselect);
                     else
-                        action = new RemoveDummyEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedDummy);
+                        if(Screen.ModelHierarchy._selectedDummy != -1)
+                            action = new RemoveDummyEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedDummy);
                     break;
 
                 case ModelEntrySelectionType.Material:
                     if (Screen.ModelHierarchy.MaterialMultiselect.HasValidMultiselection())
                         action = new RemoveMaterialEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.MaterialMultiselect);
                     else
-                        action = new RemoveMaterialEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedMaterial);
+                        if (Screen.ModelHierarchy._selectedMaterial != -1)
+                            action = new RemoveMaterialEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedMaterial);
                     break;
 
                 case ModelEntrySelectionType.GXList:
                     if (Screen.ModelHierarchy.GxListMultiselect.HasValidMultiselection())
                         action = new RemoveGXListEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.GxListMultiselect);
                     else
-                        action = new RemoveGXListEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedGXList);
+                        if (Screen.ModelHierarchy._selectedGXList != -1)
+                            action = new RemoveGXListEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedGXList);
                     break;
 
                 case ModelEntrySelectionType.Node:
                     if (Screen.ModelHierarchy.NodeMultiselect.HasValidMultiselection())
                         action = new RemoveNodeEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.NodeMultiselect);
                     else
-                        action = new RemoveNodeEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedNode);
+                        if (Screen.ModelHierarchy._selectedNode != -1)
+                            action = new RemoveNodeEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedNode);
                     break;
 
                 case ModelEntrySelectionType.Mesh:
                     if (Screen.ModelHierarchy.MeshMultiselect.HasValidMultiselection())
                         action = new RemoveMeshEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.MeshMultiselect);
                     else
-                        action = new RemoveMeshEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedMesh);
+                        if (Screen.ModelHierarchy._selectedMesh != -1)
+                            action = new RemoveMeshEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedMesh);
                     break;
 
                 case ModelEntrySelectionType.BufferLayout:
                     if (Screen.ModelHierarchy.BufferLayoutMultiselect.HasValidMultiselection())
                         action = new RemoveBufferLayoutEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.BufferLayoutMultiselect);
                     else
-                        action = new RemoveBufferLayoutEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedBufferLayout);
+                        if (Screen.ModelHierarchy._selectedBufferLayout != -1)
+                            action = new RemoveBufferLayoutEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedBufferLayout);
                     break;
 
                 case ModelEntrySelectionType.BaseSkeleton:
                     if (Screen.ModelHierarchy.BaseSkeletonMultiselect.HasValidMultiselection())
                         action = new RemoveBaseSkeletonBoneEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.BaseSkeletonMultiselect);
                     else
-                        action = new RemoveBaseSkeletonBoneEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedBaseSkeletonBone);
+                        if (Screen.ModelHierarchy._selectedBaseSkeletonBone != -1)
+                            action = new RemoveBaseSkeletonBoneEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedBaseSkeletonBone);
                     break;
 
                 case ModelEntrySelectionType.AllSkeleton:
                     if (Screen.ModelHierarchy.AllSkeletonMultiselect.HasValidMultiselection())
                         action = new RemoveAllSkeletonBoneEntryMulti(Screen, currentFlver, Screen.ModelHierarchy.AllSkeletonMultiselect);
                     else
-                        action = new RemoveAllSkeletonBoneEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedAllSkeletonBone);
+                        if (Screen.ModelHierarchy._selectedAllSkeletonBone != -1)
+                            action = new RemoveAllSkeletonBoneEntry(Screen, currentFlver, Screen.ModelHierarchy._selectedAllSkeletonBone);
                     break;
             }
 
