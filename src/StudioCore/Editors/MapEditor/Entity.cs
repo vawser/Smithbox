a3936558c342ee3900ebf0a876dfa541ce463edb
ividyon <shrinkray@gmail.com>
2024-07-28 18:29:57 +0200

    Fix: typo causing ER model masks to fail

--- src/StudioCore/Editors/MapEditor/Entity.cs
+++ src/StudioCore/Editors/MapEditor/Entity.cs
@@ -1634,7 +1634,7 @@ public class MsbEntity : Entity
                 }
                 break;
             case ProjectType.ER:
-                if (WrappedObject is MSB3.Part.EnemyBase ere)
+                if (WrappedObject is MSBE.Part.EnemyBase ere)
                 {
                     var npcParamId = ere.NPCParamID;
                     return callback(ParamBank.PrimaryBank.Params?["NpcParam"][npcParamId]);
