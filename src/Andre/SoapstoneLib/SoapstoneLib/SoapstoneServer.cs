70c622035965554723734bedab2cb508aa1ec12e
EclipsedButter <eclipsedbutter@tleaves.com>
2025-06-18 19:03:33 -0400

    Update SoapstoneServer.cs

--- src/Andre/SoapstoneLib/SoapstoneLib/SoapstoneServer.cs
+++ src/Andre/SoapstoneLib/SoapstoneLib/SoapstoneServer.cs
@@ -35,9 +35,9 @@ namespace SoapstoneLib
         /// </summary>
         public static async Task RunAsync(KnownServer server, SoapstoneServiceV1 newService, string[] args = null)
         {
-            if (OperatingSystem.IsMacOS())
+            if (!OperatingSystem.IsWindows())
             {
-                throw new NotImplementedException("SoapstoneService is not yet available on macOS.");
+                throw new NotImplementedException("SoapstoneService is not yet available on this platform.");
             }    
             if (newService == null)
             {
