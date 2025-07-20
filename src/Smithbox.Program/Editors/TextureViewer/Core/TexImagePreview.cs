352435d82685fbc71d6676755f6b514013d7368e
Vawser <ben.rouncefield@gmail.com>
2025-07-20 18:07:37 +0100

    Icon Preview
    - Disabled icon preview until redesign is implemented

--- src/Smithbox.Program/Editors/TextureViewer/Core/TexImagePreview.cs
+++ src/Smithbox.Program/Editors/TextureViewer/Core/TexImagePreview.cs
@@ -34,6 +34,9 @@ public class TexImagePreview : IResourceEventListener
     /// </summary>
     public bool DisplayImagePreview(Param.Row context, TexRef textureRef, bool displayImage = true)
     {
+        // DISABLE UNTIL FIXED
+        return false;
+
         if(textureRef == null) 
             return false;
 
